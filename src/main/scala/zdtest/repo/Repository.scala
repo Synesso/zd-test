e40c652695a2120d15709e7b2e17981bdd068fc8
Jem Mawson <jem.mawson@gmail.com>
2019-01-25 17:46:53 +1000

    Load Users into repository

--- src/main/scala/zdtest/repo/Repository.scala
+++ src/main/scala/zdtest/repo/Repository.scala
@@ -2,44 +2,51 @@ package zdtest.repo
 
 import java.io.File
 
-import zdtest.domain.Organisation
+import zdtest.domain.{Organisation, User}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
 
-case class Repository(organisations: Map[Long, Organisation] = Map.empty) {
-
-  def withOrganisations(orgs: Seq[Organisation]): Repository = copy(
-    organisations = orgs.foldLeft(organisations) { case (acc, org) =>
-        acc.updated(org._id, org)
-    }
-  )
-
-}
+class Repository(val organisations: Map[Long, Organisation] = Map.empty,
+                 val users: Map[Long, User] = Map.empty)
 
 object Repository {
+
+  // todo - no duplicates in the seqs.
+
+  def apply(orgList: Seq[Organisation] = Nil,
+            userList: Seq[User] = Nil): Repository = {
+
+    val orgMap = orgList.map(o => o._id -> o).toMap
+
+    userList.find(u => u.organization_id != -1 && !orgMap.contains(u.organization_id)).foreach { u =>
+      throw new IllegalArgumentException(s"User links to non-existent Organisation: $u")
+    }
+
+    val userMap = userList.map(u => u._id -> u).toMap
+
+    new Repository(orgMap, userMap)
+  }
 
   /**
     * Builds a repository from the data files present at the given directory
     * @param dir directory which must contain readable files users.json, tickets.json & organizations.json
     */
-  def apply(dir: File)(implicit ec: ExecutionContext): Future[Repository] = {
+  def fromDir(dir: File)(implicit ec: ExecutionContext): Future[Repository] = {
     def file(n: String) = {
       val f = new File(dir, n)
       require(f.canRead, s"Unable to read ${f.getAbsolutePath}")
       f
     }
 
+    //    val ticketsFile = file("tickets.json")
 
-    val orgsFile = file("organizations.json")
-//    val usersFile = file("users.json")
-//    val ticketsFile = file("tickets.json")
-
-    val organisations = Future(Parser.parseOrgs(orgsFile))
+    val organisationSeq = Future(Parser.parseOrgs(file("organizations.json")))
+    val userSeq = Future(Parser.parseUsers(file("users.json")))
 
     for {
-      orgs <- organisations
-    } yield Repository().withOrganisations(orgs)
+      orgs <- organisationSeq
+      users <- userSeq
+    } yield Repository(orgs, users)
   }
 
 }
--- src/test/scala/zdtest/repo/RepositorySpec.scala
+++ src/test/scala/zdtest/repo/RepositorySpec.scala
@@ -2,26 +2,35 @@ package zdtest.repo
 
 import java.io.File
 
+import org.scalacheck.Gen
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
-import zdtest.domain.{ArbitraryInput, Organisation}
+import zdtest.domain.{ArbitraryInput, Organisation, User}
 
 class RepositorySpec(implicit ee: ExecutionEnv) extends Specification with ArbitraryInput {
 
   "a repository" should {
-    val repo = Repository()
-
-    "add new organisations" >> prop { orgs: Seq[Organisation] =>
+    "map organisations" >> prop { orgs: Seq[Organisation] =>
       val withDistinctIds = orgs.zipWithIndex.map{ case (org, i) => org.copy(_id = i) }
-      val actual = repo.withOrganisations(withDistinctIds).organisations
+      val actual = Repository(withDistinctIds).organisations
       actual.values.toSeq.sortBy(_._id) mustEqual withDistinctIds
       forall(withDistinctIds) { o: Organisation => actual.get(o._id) must beSome(o) }
-    }
+    }.set(minTestsOk = 10)
+
+    "map users" >> prop { (orgs: Seq[Organisation], users: Seq[User]) =>
+      val usersMappedToOrgs = users.zip(Stream.continually(orgs).flatten).zipWithIndex.map {
+        case ((user: User, org: Organisation), id: Int) => user.copy(_id = id, organization_id = org._id)
+      }
+
+      val actual = Repository(orgs, usersMappedToOrgs).users
+      actual.values.toSeq.sortBy(_._id) mustEqual usersMappedToOrgs
+      forall(usersMappedToOrgs) { u: User => actual.get(u._id) must beSome(u) }
+    }.setGen1(Gen.nonEmptyListOf(genOrg)).set(minTestsOk = 10)
   }
 
   "instantiating a repository from files" should {
     "build correctly with sample data" >> {
-      Repository(new File("src/test/resources")) must beLike[Repository] { case repo =>
+      Repository.fromDir(new File("src/test/resources")) must beLike[Repository] { case repo =>
           repo.organisations.values must
             containTheSameElementsAs(Parser.parseOrgs(new File("src/test/resources/organizations.json")))
       }.await
